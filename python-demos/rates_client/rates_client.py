b33c1968861a5520d286d992ec3a6fbbda95061e
Eric Greene <eric@t4d.io>
2021-04-20 16:11:59 -0400

    add code demos

--- python-demos/rates_client/rates_client.py
+++ python-demos/rates_client/rates_client.py
@@ -1,26 +1,39 @@
 """ rates client module """
 
 from datetime import date
-import requests
 import time
+import threading
+import requests
 
 from datetime_demos.business_days import business_days
+
+def get_rates(working_day: date, responses: list[str]) -> None:
+    """ get rates """
+
+    working_day_str = working_day.strftime("%Y-%m-%d")
+    url = "".join([
+        "https://api.ratesapi.io/api/",
+        # "http://localhost:5000/api/",
+        working_day_str,
+        "?base=USD&symbols=EUR"
+    ])
+    response = requests.request("GET", url)
+    responses.append(response.text)
 
 def main() -> None:
     """ main """
 
     responses: list[str] = []
+    threads: list[threading.Thread] = []
 
-    for working_day in business_days(date(2019,1,1), date(2019,1,31)):
-        working_day_str = working_day.strftime("%Y-%m-%d")
-        url = "".join([
-            "https://api.ratesapi.io/api/",
-            # "http://localhost:5000/api/",
-            working_day_str,
-            "?base=USD&symbols=EUR"
-        ])
-        response = requests.request("GET", url)
-        responses.append(response.text)
+    for working_day in business_days(date(2019,1,1), date(2019,2,28)):
+        a_thread = threading.Thread(
+            target=get_rates, args=(working_day, responses))
+        a_thread.start()
+        threads.append(a_thread)
+
+    for a_thread in threads:
+        a_thread.join()
 
     print("\n".join(responses))
 
