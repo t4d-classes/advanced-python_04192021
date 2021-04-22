106ec327122357e7bbea0557d130f0127555e3e6
Eric Greene <eric@t4d.io>
2021-04-22 15:33:49 -0400

    add simple connection

--- python-demos/db_demos/simple_connection.py
+++ python-demos/db_demos/simple_connection.py
@@ -9,17 +9,31 @@ local_trust_conn_options = [
     "Trusted_Connection=yes",
 ]
 
+# local_docker_conn_options = [
+#     "DRIVER={ODBC Driver 17 for SQL Server}",
+#     "SERVER=localhost,11433",
+#     "DATABASE=ratesapp",
+#     "UID=sa",
+#     "PWD=sqlDbp@ss!"
+# ]
+
+# local_azure_conn_options = [
+#     "DRIVER={ODBC Driver 17 for SQL Server}",
+#     "SERVER=ewgpythonclass.database.windows.net",
+#     "DATABASE=rates",
+#     "UID=dbuser",
+#     "PWD=sqlDbp@ss!"
+# ]
+
 conn_string = ";".join(local_trust_conn_options)
 
 def main() -> None:
     """ main """
 
     with pyodbc.connect(conn_string) as con:
-        rates = con.execute("select * from rates")
-
+        rates = con.execute("select ClosingDate as closing_date from rates")
         for rate in rates:
-            print(rate)
+            print(rate.closing_date)
 
 if __name__ == "__main__":
     main()
-    
